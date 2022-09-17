cd1ae2a27e18ad611f9efb7da5f0ee5cdd1af6bc
bony075 <bony.075@gmail.com>
2022-09-17 11:07:00 +0600

    lock inspect website element

--- js/script.js
+++ js/script.js
@@ -25,4 +25,50 @@ const sr = ScrollReveal ({
 
 sr.reveal('.home-text',{delay:190, origin:'bottom'})
 
-sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.about,.services,.portfolio,.contact', { delay: 200, origin: 'bottom' });
+// lock inspect
+
+
+
+/**
+	* Disable right-click of mouse, F12 key, and save key combinations on page
+	*/
+	document.addEventListener("contextmenu", function(e){
+		e.preventDefault(); 
+ }, false);
+	document.addEventListener("keydown", function(e) {
+ //document.onkeydown = function(e) { 
+ // "I" key 
+ if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+		disabledEvent(e); 
+ }
+	// "J" key 
+	if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+		disabledEvent(e); 
+ }
+	// "S" key + macOS 
+	if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+		disabledEvent(e); 
+ }
+	// "U" key 
+	if (e.ctrlKey && e.keyCode == 85) {
+		disabledEvent(e); 
+ }
+	// "F12" key 
+	if (event.keyCode == 123) {
+		disabledEvent(e); 
+ }
+	// "C" key 
+	if (e.ctrlKey && event.keyCode == 67) {
+		disabledEvent(e); 
+ } 
+ }, false);
+	function disabledEvent(e){ 
+ if (e.stopPropagation){
+		e.stopPropagation(); 
+ } else if (window.event){
+		window.event.cancelBubble = true; 
+ }
+	e.preventDefault();
+	return false; 
+ }
