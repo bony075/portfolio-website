c7389e560507ed65ad99c0f3e59a41b31fe445cb
Bony <bony.075@gmail.com>
2022-12-10 23:49:14 +0600

    change project name

---
+++ _backup.js
@@ -0,0 +1,96 @@
+	// <div class="row container">
+	// 			<div class="box box1">
+	// 				<!-- <img src="img/1.png"> -->
+	// 			</div>
+	// 			<div class="main-row">
+	// 				<div class="row-text">
+	// 					<h6>Development</h6>
+	// 				</div>
+	// 				<div class="row-icon">
+	// 					<i class='bx bx-heart'></i>
+	// 				</div>
+	// 			</div>
+	// 			<h3>Mobile app landing design & Services<a href="https://the-real-fighter.netlify.app" class="btnDetail"
+	// 					target="_blank">Live Preview</a></h3>
+
+	// 		</div>
+	// 		<div class="row container">
+	// 			<div class="box box2">
+	// 				<!-- <img src="img/1.png"> -->
+	// 			</div>
+	// 			<div class="main-row">
+	// 				<div class="row-text">
+	// 					<h6>Development</h6>
+	// 				</div>
+	// 				<div class="row-icon">
+	// 					<i class='bx bx-heart'></i>
+	// 				</div>
+	// 			</div>
+	// 			<h3>Mobile app landing design & Services<a href="https://the-real-fighter.netlify.app" class="btnDetail"
+	// 					target="_blank">Live Preview</a></h3>
+
+	// 		</div>
+	// 		<div class="row container">
+	// 			<div class="box box3">
+	// 				<!-- <img src="img/1.png"> -->
+	// 			</div>
+	// 			<div class="main-row">
+	// 				<div class="row-text">
+	// 					<h6>Development</h6>
+	// 				</div>
+	// 				<div class="row-icon">
+	// 					<i class='bx bx-heart'></i>
+	// 				</div>
+	// 			</div>
+	// 			<h3>Mobile app landing design & Services<a href="https://the-real-fighter.netlify.app" class="btnDetail"
+	// 					target="_blank">Live Preview</a></h3>
+
+	// 		</div>
+	// 		<div class="row container">
+	// 			<div class="box box4">
+	// 				<!-- <img src="img/1.png"> -->
+	// 			</div>
+	// 			<div class="main-row">
+	// 				<div class="row-text">
+	// 					<h6>Development</h6>
+	// 				</div>
+	// 				<div class="row-icon">
+	// 					<i class='bx bx-heart'></i>
+	// 				</div>
+	// 			</div>
+	// 			<h3>Mobile app landing design & Services<a href="https://the-real-fighter.netlify.app" class="btnDetail"
+	// 					target="_blank">Live Preview</a></h3>
+
+	// 		</div>
+	// 		<div class="row container">
+	// 			<div class="box box5">
+	// 				<!-- <img src="img/1.png"> -->
+	// 			</div>
+	// 			<div class="main-row">
+	// 				<div class="row-text">
+	// 					<h6>Development</h6>
+	// 				</div>
+	// 				<div class="row-icon">
+	// 					<i class='bx bx-heart'></i>
+	// 				</div>
+	// 			</div>
+	// 			<h3>Mobile app landing design & Services<a href="https://the-real-fighter.netlify.app" class="btnDetail"
+	// 					target="_blank">Live Preview</a></h3>
+
+	// 		</div>
+	// 		<div class="row container">
+	// 			<div class="box box6">
+	// 				<!-- <img src="img/1.png"> -->
+	// 			</div>
+	// 			<div class="main-row">
+	// 				<div class="row-text">
+	// 					<h6>Development</h6>
+	// 				</div>
+	// 				<div class="row-icon">
+	// 					<i class='bx bx-heart'></i>
+	// 				</div>
+	// 			</div>
+	// 			<h3>Mobile app landing design & Services<a href="https://the-real-fighter.netlify.app" class="btnDetail"
+	// 					target="_blank">Live Preview</a></h3>
+
+	// 		</div>
